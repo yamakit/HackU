df313e2b49cdea8545dd9cbb2f33073680d9e826
yamakit <yamakitta@gmail.com>
2021-12-14 19:31:44 +0900

    php8.0.6でcomposer.jsonを作成

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b2e5717a64bd2156c37310d7a2a263db6b838222',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b2e5717a64bd2156c37310d7a2a263db6b838222',
+            'dev_requirement' => false,
+        ),
+    ),
+);
